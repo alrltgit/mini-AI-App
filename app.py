b72f38eb7fd6df6ab97fa79deb99feb8ce84f39f
Alina <ralcevichalina70@gmail.com>
2025-07-29 11:05:21 +0200

    Changes the design of the app to chat; added try-except block to handle APIError in the API request.

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask, request, render_template, g
+from flask import Flask, request, render_template, g, session
 from google import genai
+from google.genai import errors
 import sqlite3
 from create_db import create_database
 import os
-
 app = Flask(__name__)
 client = genai.Client()
-
+app.secret_key = os.environ.get('session_secret_key')
 @app.route('/')
 def index():
+    session.clear()
     return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
     prompt = request.form['prompt']
-    response = client.models.generate_content(
-        model = 'gemini-2.5-flash',
-        contents = prompt
-    )
+    try:
+        response = client.models.generate_content(
+            model = 'gemini-2.5-flash',
+            contents = prompt
+        )
+    except errors.APIError as e:
+        response = f"Error: {e}"
     if not os.path.isfile('/Users/apple/PycharmProjects/mini-AI-App/data.db'):
         create_database()
     db = get_db()
@@ -28,7 +32,11 @@ def generate():
     # for row in rows:
     #    print (row)
     reply = response.text
-    return render_template('response.html', response = reply)
+    if 'chat' not in session:
+        session['chat'] = []
+    session['chat'].append({'prompt': prompt, 'reply': reply})
+    session.modified = True
+    return render_template('index.html', chat = session['chat'])
 
 def get_db():
     db = getattr(g, '_database', None)
